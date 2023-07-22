d77ccf8f28d12bf16765c3773dc708044b77e584
ishilbi <107364357+ishilbi@users.noreply.github.com>
2023-07-21 20:25:38 -0400

    Added the functionality for the select

--- javascript/index.js
+++ javascript/index.js
@@ -1,20 +1,49 @@
 function updateTime() {
   let roanokeElement = document.querySelector("#roanoke");
-  let roanokeDateElement = roanokeElement.querySelector(".date");
-  let roanokeTimeElement = roanokeElement.querySelector(".time");
-  let roanokeTime = moment().tz("America/New_York");
-  roanokeDateElement.innerHTML = roanokeTime.format("MMMM Do YYYY");
-  roanokeTimeElement.innerHTML = roanokeTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-  let seoulElement = document.querySelector("#seoul");
-  let seoulDateElement = seoulElement.querySelector(".date");
-  let seoulTimeElement = seoulElement.querySelector(".time");
-  let seoulTime = moment().tz("Asia/Seoul");
+  if (roanokeElement) {
+    let roanokeDateElement = roanokeElement.querySelector(".date");
+    let roanokeTimeElement = roanokeElement.querySelector(".time");
+    let roanokeTime = moment().tz("America/New_York");
+    roanokeDateElement.innerHTML = roanokeTime.format("MMMM Do YYYY");
+    roanokeTimeElement.innerHTML = roanokeTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 
-  seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
-  seoulTimeElement.innerHTML = seoulTime.format("h:mm:ss [<small>]A[</small>]");
+  let seoulElement = document.querySelector("#seoul");
+  if (seoulElement) {
+    let seoulDateElement = seoulElement.querySelector(".date");
+    let seoulTimeElement = seoulElement.querySelector(".time");
+    let seoulTime = moment().tz("Asia/Seoul");
+
+    seoulDateElement.innerHTML = seoulTime.format("MMMM Do YYYY");
+    seoulTimeElement.innerHTML = seoulTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
+}
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+          <div>
+            <h2>${cityName}</h2>
+            <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+          </div>
+          <div>
+            <div class="time">${cityTime.format(
+              "h:mm:ss"
+            )}<small>${cityTime.format("A")} </small></div>
+          </div>
+        </div>
+  `;
 }
 updateTime;
 setInterval(updateTime, 1000);
-//Roanoke
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change", updateCity);
